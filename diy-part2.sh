f2313475d0c3dcd0b10bddbfcf75468223222d3a
wuiiled <32694740+wuiiled@users.noreply.github.com>
2021-09-30 14:10:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,9 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+
+# 修改连接数
+sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=16384/g' package/qca/nss/qca-nss-ecm-64/Makefile
+sed -i 's/dev.nss.ipv4cfg.ipv4_conn=.*/dev.nss.ipv4cfg.ipv4_conn=8192/g' package/qca/nss/qca-nss-drv-64/files/qca-nss-drv.sysctl
+sed -i 's/dev.nss.ipv6cfg.ipv6_conn=.*/dev.nss.ipv6cfg.ipv6_conn=8192/g' package/qca/nss/qca-nss-drv-64/files/qca-nss-drv.sysctl
